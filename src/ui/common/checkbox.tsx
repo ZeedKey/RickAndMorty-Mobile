3de457c658bec0bf59a99d0c8d0c2c07b57e16e9
Yaroslav Volokh <zlint@Mac-mini-Yaroslav.local>
2022-05-19 16:18:06 +0400

    Checkbox styles fixed

--- src/ui/common/checkbox.tsx
+++ src/ui/common/checkbox.tsx
@@ -1,23 +1,28 @@
-import CheckBox from '@react-native-community/checkbox'
-import { accent } from '@theme'
-import React from 'react'
-import styled from 'styled-components/native'
+import CheckBox from '@react-native-community/checkbox';
+import {accent} from '@theme';
+import React from 'react';
+import styled from 'styled-components/native';
 
-export const Checkbox: React.FC<{ isActive?: boolean }> = ({ isActive }) => {
+interface ICheckBoxProps {
+  isActive: boolean;
+}
+
+export const Checkbox: React.FC<ICheckBoxProps> = ({isActive}) => {
   return (
     <StyledCheckBox
-    disabled={true}
+      disabled={true}
       onFillColor={accent.indigo}
       tintColor={accent.indigo}
       onCheckColor="white"
+      offAnimationType="fill"
       onTintColor={accent.indigo}
       value={isActive}
     />
-  )
-}
+  );
+};
 
 const StyledCheckBox = styled(CheckBox)`
   width: 20px;
   height: 20px;
   margin-right: 15px;
-`
+`;
